a3e1474d4bd1a2cb8b978643bfdf2dbda546888f
Jean-Marie Lemetayer <jeanmarie.lemetayer@gmail.com>
2016-02-15 10:21:11 +0100

    Add global js script

---
+++ dist/js/script.js
@@ -0,0 +1,15 @@
+---
+---
+{% include js/jquery.min.js %}
+{% include js/bootstrap/transition.js %}
+{% include js/bootstrap/alert.js %}
+{% include js/bootstrap/button.js %}
+{% include js/bootstrap/carousel.js %}
+{% include js/bootstrap/collapse.js %}
+{% include js/bootstrap/dropdown.js %}
+{% include js/bootstrap/modal.js %}
+{% include js/bootstrap/tooltip.js %}
+{% include js/bootstrap/popover.js %}
+{% include js/bootstrap/scrollspy.js %}
+{% include js/bootstrap/tab.js %}
+{% include js/bootstrap/affix.js %}
