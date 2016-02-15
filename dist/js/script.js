cd5a71e1a7789f3828ece5f669bcaffbda6a6154
Jean-Marie Lemetayer <jeanmarie.lemetayer@gmail.com>
2016-02-15 20:31:42 +0100

    Add abba script in dist file

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,15 +1,26 @@
 ---
 ---
 {% include js/jquery.min.js %}
-{% include js/bootstrap/transition.js %}
-{% include js/bootstrap/alert.js %}
-{% include js/bootstrap/button.js %}
-{% include js/bootstrap/carousel.js %}
-{% include js/bootstrap/collapse.js %}
-{% include js/bootstrap/dropdown.js %}
-{% include js/bootstrap/modal.js %}
-{% include js/bootstrap/tooltip.js %}
-{% include js/bootstrap/popover.js %}
-{% include js/bootstrap/scrollspy.js %}
-{% include js/bootstrap/tab.js %}
-{% include js/bootstrap/affix.js %}
+{% comment %}{% include js/bootstrap/transition.js %}{% endcomment %}
+{% comment %}{% include js/bootstrap/alert.js %}{% endcomment %}
+{% comment %}{% include js/bootstrap/button.js %}{% endcomment %}
+{% comment %}{% include js/bootstrap/carousel.js %}{% endcomment %}
+{% comment %}{% include js/bootstrap/collapse.js %}{% endcomment %}
+{% comment %}{% include js/bootstrap/dropdown.js %}{% endcomment %}
+{% comment %}{% include js/bootstrap/modal.js %}{% endcomment %}
+{% comment %}{% include js/bootstrap/tooltip.js %}{% endcomment %}
+{% comment %}{% include js/bootstrap/popover.js %}{% endcomment %}
+{% comment %}{% include js/bootstrap/scrollspy.js %}{% endcomment %}
+{% comment %}{% include js/bootstrap/tab.js %}{% endcomment %}
+{% comment %}{% include js/bootstrap/affix.js %}{% endcomment %}
+$(document).ready(function() {
+	/* Use Bootstrap table */
+	$('table').addClass('table table-hover');
+	/* Remove align="right" */
+	$('td').removeAttr('align');
+	/* Move table header in thead */
+	var header = $('table tbody tr').first();
+	$('table').prepend($('<thead/>').append(header));
+	/* Show table */
+	$('#table').show();
+});
