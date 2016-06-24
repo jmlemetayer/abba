e182fa74e74faca1eee507829f6e680ba3413a00
Jean-Marie Lemetayer <jeanmarie.lemetayer@gmail.com>
2016-06-24 12:02:26 +0200

    Propagate the sorting filter

--- dist/js/script.js
+++ dist/js/script.js
@@ -24,6 +24,7 @@ $(document).ready(function() {
 	var header = $('table tbody tr').first();
 	$('table').prepend($('<thead/>').append(header));
 	/* Add sorting arrows */
+	var filter;
 	var name = $('a', header).eq(0);
 	var date = $('a', header).eq(1);
 	var size = $('a', header).eq(2);
@@ -31,18 +32,27 @@ $(document).ready(function() {
 		name.attr('href', '.');
 	}
 	if(/\/$/.test(document.URL) || /\?C=N;O=A$/.test(document.URL)) {
+		filter = '';
 		name.append('<span class="icon icon-down">');
 	} else if(/\?C=N;O=D$/.test(document.URL)) {
+		filter = '?C=N;O=D';
 		name.append('<span class="icon icon-up">');
 	} else if(/\?C=M;O=A$/.test(document.URL)) {
+		filter = '?C=M;O=A';
 		date.append('<span class="icon icon-down">');
 	} else if(/\?C=M;O=D$/.test(document.URL)) {
+		filter = '?C=M;O=D';
 		date.append('<span class="icon icon-up">');
 	} else if(/\?C=S;O=A$/.test(document.URL)) {
+		filter = '?C=S;O=A';
 		size.append('<span class="icon icon-down">');
 	} else if(/\?C=S;O=D$/.test(document.URL)) {
+		filter = '?C=S;O=D';
 		size.append('<span class="icon icon-up">');
 	}
+	/* Update home link */
+	var home = $('a.navbar-brand');
+	home.attr('href', home.attr('href') + filter);
 	/* Parse each row */
 	$('table tbody tr').each(function(i) {
 		var a = $('a', this).first();
@@ -56,6 +66,7 @@ $(document).ready(function() {
 		} else if (name.substring(name.length - 1) === '/') {
 			$(this).addClass('active');
 			$('td', this).last().html('');
+			a.attr('href', a.attr('href') + filter);
 			a.prepend('<span class="icon icon-folder">');
 		/* Set file row style */
 		} else {
@@ -71,10 +82,10 @@ $(document).ready(function() {
 	var directories = document.URL.split('/').slice(3, -1);
 	if (directories.length > 0) {
 		var href = '/';
-		$('.breadcrumb li').removeAttr('class').wrapInner('<a href="' + href + '">');
+		$('.breadcrumb li').removeAttr('class').wrapInner('<a href="' + href + filter + '">');
 		$.each(directories, function(i ,directory) {
 			href += directory + '/';
-			$('.breadcrumb').append('<li><a href="' + href + '">' + decodeURIComponent(directory));
+			$('.breadcrumb').append('<li><a href="' + href + filter + '">' + decodeURIComponent(directory));
 		});
 		$('.breadcrumb li').last().addClass('active').children('a').contents().unwrap();
 	}
