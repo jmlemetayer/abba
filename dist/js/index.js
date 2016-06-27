e9a24742c810c7e6538f2fb4c34c7b43188566fe
Jean-Marie Lemetayer <jeanmarie.lemetayer@gmail.com>
2016-06-27 14:37:35 +0200

    Add empty page, refs #3

---
+++ dist/js/index.js
@@ -0,0 +1,16 @@
+---
+layout:
+---
+{% include js/jquery.min.js %}
+{% comment %}{% include js/bootstrap/transition.js %}{% endcomment %}
+{% comment %}{% include js/bootstrap/alert.js %}{% endcomment %}
+{% comment %}{% include js/bootstrap/button.js %}{% endcomment %}
+{% comment %}{% include js/bootstrap/carousel.js %}{% endcomment %}
+{% comment %}{% include js/bootstrap/collapse.js %}{% endcomment %}
+{% comment %}{% include js/bootstrap/dropdown.js %}{% endcomment %}
+{% comment %}{% include js/bootstrap/modal.js %}{% endcomment %}
+{% comment %}{% include js/bootstrap/tooltip.js %}{% endcomment %}
+{% comment %}{% include js/bootstrap/popover.js %}{% endcomment %}
+{% comment %}{% include js/bootstrap/scrollspy.js %}{% endcomment %}
+{% comment %}{% include js/bootstrap/tab.js %}{% endcomment %}
+{% comment %}{% include js/bootstrap/affix.js %}{% endcomment %}
