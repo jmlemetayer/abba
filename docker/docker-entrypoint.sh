efd78cb4eaaf18a0a2713860c57e18e4967e9d0e
Jean-Marie Lemetayer <jeanmarie.lemetayer@gmail.com>
2020-09-08 23:06:45 +0200

    docker: run uninstall on exit

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -8,4 +8,12 @@ curl -sL ${BASE_URL:-https://jmlemetayer.github.io/abba}/install -o /tmp/install
 # And run it in the workdir
 /bin/sh -e ${DEBUG:+-x} /tmp/install
 
-exec "$@"
+# Uninstall on exit
+trap "[ -x .abba/uninstall ] && .abba/uninstall" EXIT
+
+# Execute the command in background to be able to call the uninstall script
+# Using SIGWINCH as it is used by httpd for graceful exit
+exec "$@" &
+trap "kill $!" TERM
+trap "kill -WINCH $!" WINCH
+wait
