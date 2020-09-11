493d2a2aca4643590ae2b6164a73b930da088c0b
Jean-Marie Lemetayer <jeanmarie.lemetayer@gmail.com>
2020-09-11 16:01:42 +0200

    js: handle the icon column
    - Use <th> for the icon column
    - Read the mime-type from the old icon
    - Replace the old icon by a fontawesome icon
    - Handle special rows

--- _includes/js/abba/main.js
+++ _includes/js/abba/main.js
@@ -1,8 +1,11 @@
 // Use Bootstrap table
 $("table").addClass("table table-hover").wrap("<div class='table-responsive'>");
 
-// Remove align="right"
-$("td").removeAttr("align");
+// Remove valign="top" and align="right"
+$("td, th").removeAttr("align").removeAttr("valign");
+
+// Replace the first <td> into <th>
+$("td:first-child").changeElementType("<th/>");
 
 // Move all rows in <tbody>
 $("table").prependIfNotExist("tbody", "<tbody/>").append($("tr"));
@@ -10,9 +13,52 @@ $("table").prependIfNotExist("tbody", "<tbody/>").append($("tr"));
 // Move table header in <thead>
 $("table").prependIfNotExist("thead", "<thead/>").append($("tr").first());
 
+// Clear the icon column of the header
+$("th").first().empty();
+
 // Trim each cells inner html
 $("td, th").each(function() {
 	$(this).html($(this).html().trim());
+});
+
+const icon_classe_by_type = {
+	"default": "fa-fw far fa-file",
+	"directory": "fa-fw fas fa-folder",
+	"archive": "fa-fw far fa-file-archive",
+	"audio": "fa-fw far fa-file-audio",
+	"code": "fa-fw far fa-file-code",
+	"excel": "fa-fw far fa-file-excel",
+	"image": "fa-fw far fa-file-image",
+	"pdf": "fa-fw far fa-file-pdf",
+	"powerpoint": "fa-fw far fa-file-powerpoint",
+	"text": "fa-fw far fa-file-alt",
+	"video": "fa-fw far fa-file-video",
+	"word": "fa-fw far fa-file-word",
+};
+
+// Parse each row
+$("tbody > tr").each(function(index) {
+	var icon = $("th", this).first();
+	var size = $("td", this).last();
+
+	// Read the type from the <img> alt text
+	var type = $("img", icon).attr("alt").replace(/[\[\] ]+/g, "");
+
+	// Replace the icon
+	if (type in icon_classe_by_type) {
+		icon.html("<i class='" + icon_classe_by_type[type] + "'/>");
+	} else {
+		icon.html("<i class='" + icon_classe_by_type["default"] + "'/>");
+	}
+
+	// Remove the parent row
+	if (type === "parent") {
+		$(this).remove();
+
+	// Update directory style
+	} else if (type === "directory") {
+		size.empty();
+	}
 });
 
 // Show main
--- _includes/js/abba/utils.js
+++ _includes/js/abba/utils.js
@@ -1,4 +1,10 @@
 (function($) {
+	$.fn.changeElementType = function(element) {
+		this.replaceWith(function() {
+			return $(element).append($(this).contents());
+		});
+	};
+
 	$.fn.prependIfNotExist = function(selector, element) {
 		if ($(selector, this).length === 0) {
 			return $(element).prependTo(this);
