4dea6b88dcc907d4f0b86bca154100def6fc9e03
Jean-Marie Lemetayer <jeanmarie.lemetayer@gmail.com>
2020-09-11 11:10:31 +0200

    add initial js distribution file

---
+++ _includes/js/abba/main.js
@@ -0,0 +1,2 @@
+// Use Bootstrap table
+$("table").addClass("table table-hover").wrap("<div class='table-responsive'>");
---
+++ abba/js/script.js
@@ -0,0 +1,6 @@
+---
+---
+{% include js/jquery/jquery.slim.min.js %}
+$(document).ready(function() {
+{% include js/abba/main.js %}
+});
