bb0670fdb8f072a4b8f15bbc4c6725f9b57af8db
Jean-Marie Lemetayer <jeanmarie.lemetayer@gmail.com>
2020-09-25 14:36:03 +0200

    js: include popper and bootstrap in the script

--- abba/js/script.js
+++ abba/js/script.js
@@ -1,7 +1,9 @@
 ---
 ---
 {% include js/jquery/jquery.slim.min.js %}
+{% include js/popperjs/popper.min.js %}
 {% include js/moment/moment.min.js %}
+{% include js/bootstrap/bootstrap.min.js %}
 {% include js/abba/utils.js %}
 $(document).ready(function() {
 {% include js/abba/main.js %}
