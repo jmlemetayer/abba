1a04f8e146aa506f35d7e7bb67424989d7e84247
Jean-Marie Lemetayer <jeanmarie.lemetayer@gmail.com>
2020-09-28 09:49:38 +0200

    js: handle the error page back button

---
+++ _includes/js/abba/error.js
@@ -0,0 +1,2 @@
+// Configure the Back button href
+$(".jumbotron a.btn").attr("href", document.referrer);
--- _includes/js/abba/main.js
+++ _includes/js/abba/main.js
@@ -2,5 +2,9 @@ if ($("table")) {
 {% include js/abba/autoindex.js %}
 }
 
+if ($(".jumbotron")) {
+{% include js/abba/error.js %}
+}
+
 // Show the <main>
 $("main").show();
