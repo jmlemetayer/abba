04b846c489205d439546d88dc2f5271e5a179948
Jean-Marie Lemetayer <jeanmarie.lemetayer@gmail.com>
2020-10-06 19:34:40 +0200

    docker: allow the theme to be provided by the environment

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -6,7 +6,7 @@
 curl -sL ${BASE_URL:-https://jmlemetayer.github.io/abba}/install -o /tmp/install
 
 # And run it in the workdir
-/bin/sh -e ${DEBUG:+-x} /tmp/install
+/bin/sh -e ${DEBUG:+-x} /tmp/install ${THEME}
 
 # Uninstall on exit
 trap "[ -x .abba/uninstall ] && .abba/uninstall" EXIT
