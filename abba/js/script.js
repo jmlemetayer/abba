0147f48c17e26624fe506c45322aab74398cfad0
Jean-Marie Lemetayer <jeanmarie.lemetayer@gmail.com>
2021-06-10 13:30:18 +0200

    popperjs: already bundled in bootstrap

--- abba/js/script.js
+++ abba/js/script.js
@@ -1,7 +1,6 @@
 ---
 ---
 {% include js/jquery/jquery.slim.min.js %}
-{% include js/popperjs/popper.min.js %}
 {% include js/moment/moment.min.js %}
 {% include js/bootstrap/bootstrap.min.js %}
 {% include js/abba/utils.js %}
